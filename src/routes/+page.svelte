b380486def0cfda7785fad0676999d1733c4ec79
Gabriele Corti <borntofrappe@gmail.com>
2025-05-24 17:28:39 +0200

    note styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 
 <Icons />
 
-<main>
-  <nav aria-label="App menu">
+<main class="note">
+  <nav class="note--start" aria-label="App menu">
     <!-- new note, options, close note  -->
     <button aria-label="New note" title="New note">
       <svg width="1em" height="1em" viewBox="0 0 1 1">
@@ -24,8 +24,8 @@
       </svg>
     </button>
   </nav>
-  <textarea placeholder="Take a note..."></textarea>
-  <nav aria-label="App menu">
+  <textarea class="note--mid" placeholder="Take a note..."></textarea>
+  <nav class="note--end" aria-label="App menu">
     <button aria-pressed="false" aria-label="Bold" title="Bold">
       <svg width="1em" height="1em" viewBox="0 0 1 1">
         <use href="#bold" />
@@ -66,3 +66,109 @@
     </button>
   </nav>
 </main>
+
+<style>
+  .note {
+    width: 385px;
+    height: 400px;
+    --button-size-1: 30px;
+    --button-size-2: 35px;
+    --border-radius: 10px;
+    --ui-padding: 8px;
+    --button-padding: 7px;
+
+    --color-text-1: #ededed;
+    --color-text-2: #c1c1c1;
+    --color-surface-1: #333333;
+    --color-surface-2: #454545;
+
+    color: var(--color-text-1);
+    background: var(--color-surface-1);
+    border-radius: var(--border-radius);
+    display: flex;
+    flex-direction: column;
+
+    > .note--mid {
+      flex-grow: 1;
+    }
+  }
+
+  .note--start {
+    --button-size: var(--button-size-2);
+    border-start-start-radius: var(--border-radius);
+    border-start-end-radius: var(--border-radius);
+    color: var(--color-text-2);
+    background: var(--color-surface-2);
+    display: flex;
+    align-items: center;
+
+    & *:first-child {
+      margin-inline-end: auto;
+      border-start-start-radius: inherit;
+    }
+    & *:last-child {
+      border-start-end-radius: inherit;
+    }
+  }
+
+  .note--end {
+    color: var(--color-text-2);
+    --button-size: var(--button-size-1);
+    padding: var(--ui-padding);
+    border-end-start-radius: var(--border-radius);
+    border-end-end-radius: var(--border-radius);
+    border-block-start: 1px solid var(--color-surface-2);
+  }
+
+  textarea {
+    font-size: 1.05em;
+    border: none;
+    color: inherit;
+    background: none;
+    resize: none;
+    padding: var(--ui-padding);
+    line-height: 1.5;
+    scrollbar-width: thin;
+    scrollbar-color: var(--color-surface-2) transparent;
+
+    &:focus {
+      outline: none;
+    }
+
+    &::placeholder {
+      color: var(--color-text-2);
+    }
+  }
+
+  button {
+    cursor: pointer;
+    --_button-size: var(--button-size, 26px);
+    width: var(--_button-size);
+    height: var(--_button-size);
+    padding: var(--button-padding, 6px);
+    border: none;
+    background: none;
+    color: inherit;
+
+    & > svg {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover {
+      background: oklch(from currentColor l c h / 0.1);
+    }
+
+    position: relative;
+    /* will work on pressed buttons */
+    /*
+    &[aria-pressed="true"]::before {
+      content: "";
+      position: absolute;
+      inset: 0;
+      background: oklch(from currentColor l c h / 0.1);
+    }
+     */
+  }
+</style>
