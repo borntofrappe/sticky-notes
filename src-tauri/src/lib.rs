07d2830ccdd12885f9ed349925e222985da33ee6
Gabriele Corti <borntofrappe@gmail.com>
2025-05-25 21:50:36 +0200

    tauri store plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn close_note(window: tauri::Window) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![close_note])
         .run(tauri::generate_context!())
