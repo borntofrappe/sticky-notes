c8d4e621f9fb0a495dc00c3f879227bd0ff3a5c5
Gabriele Corti <borntofrappe@gmail.com>
2025-05-26 10:00:40 +0200

    new note
    create another window copying the width and height of the invoking one; async function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,37 @@
-use tauri::Manager;
+use tauri::{LogicalSize, Manager};
 
 #[tauri::command]
 fn close_note(window: tauri::Window) {
     let label = window.label();
     let _ = window.get_webview_window(&label).unwrap().close();
+}
+
+#[tauri::command]
+async fn new_note(window: tauri::Window) {
+    let label = window.label();
+    let webview_window = window.get_webview_window(&label).unwrap();
+    let inner_size = webview_window.inner_size().unwrap();
+    let scale_factor = webview_window
+        .current_monitor()
+        .unwrap()
+        .map(|m| m.scale_factor())
+        .unwrap_or(1.);
+    let size: LogicalSize<u32> = inner_size.to_logical(scale_factor);
+
+    let width: f64 = size.width.into();
+    let height: f64 = size.height.into();
+    let new_label = "note-0".to_string();
+
+    let _ = tauri::WebviewWindowBuilder::new(
+        &window,
+        new_label,
+        tauri::WebviewUrl::App("index.html".into()),
+    )
+    .inner_size(width, height)
+    .transparent(true)
+    .decorations(false)
+    .build()
+    .unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -25,7 +53,7 @@ pub fn run() {
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![close_note])
+        .invoke_handler(tauri::generate_handler![close_note, new_note])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
