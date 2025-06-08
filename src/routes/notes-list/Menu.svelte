7936ab7950030800ea3f815e1bba600bef73c540
Gabriele Corti <borntofrappe@gmail.com>
2025-06-08 21:58:33 +0200

    notes list menu
    include data-tauri-drag region attribute

---
+++ src/routes/notes-list/Menu.svelte
@@ -0,0 +1,20 @@
+<nav data-tauri-drag-region class="notes-list--menu" aria-label="Menu">
+  <button
+    aria-label="New note"
+    class="icon-button"
+    data-anchor="bottom left"
+  >
+    <svg width="1em" height="1em" viewBox="0 0 1 1">
+      <use href="#icon-add" />
+    </svg>
+  </button>
+  <button
+    aria-label="Close window"
+    class="icon-button"
+    data-anchor="bottom right"
+  >
+    <svg width="1em" height="1em" viewBox="0 0 1 1">
+      <use href="#icon-close" />
+    </svg>
+  </button>
+</nav>
