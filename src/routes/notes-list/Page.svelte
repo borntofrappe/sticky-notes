70c89caa62d17d54f28977e408e1f4eded55d784
Gabriele Corti <borntofrappe@gmail.com>
2025-06-08 22:05:49 +0200

    notes list markup

--- src/routes/notes-list/Page.svelte
+++ src/routes/notes-list/Page.svelte
@@ -5,6 +5,10 @@
 
   import { DB_PATH } from "$lib/constants";
 
+  import Menu from "./Menu.svelte";
+  import Placeholder from "./Placeholder.svelte";
+  import LastModified from "./LastModified.svelte";
+
   let notes: Note[] = $state([]);
 
   onMount(async () => {
@@ -22,4 +26,24 @@
   };
 </script>
 
-{JSON.stringify(sort(notes))}
+<div class="notes-list">
+  <Menu />
+
+  <main class="notes-list--content">
+    <h1>Sticky notes</h1>
+
+    {#if notes.length === 0}
+      <figure class="notes-list--figure">
+        <Placeholder />
+        <figcaption>Tap the new note button above to create a note</figcaption>
+      </figure>
+    {:else}
+      {#each sort(notes) as note}
+        <div class="notes-list--note" data-color={note.highlight}>
+          <LastModified dateString={note.lastModified} />
+          <div>{@html note.text}</div>
+        </div>
+      {/each}
+    {/if}
+  </main>
+</div>
